54ae9f11c976ca02a615b0d79c0ff4be7d2e0539
C-odingNomad <zeeblak@yahoo.com>
2023-11-10 00:58:14 +0500

    Completed 4th Exercise

--- ex03/index.ts
+++ ex03/index.ts
@@ -1,1 +1,1 @@
-console.log(`Quaid e Azam once said, “Think a hundred times before you take a decision, but once that decision is taken, stand by it as one man.”`)
+console.log(`Quaid e Azam once said, "Think a hundred times before you take a decision, but once that decision is taken, stand by it as one man."`)
---
+++ ex04/index.ts
@@ -0,0 +1,3 @@
+var famous_person: string = "Quaid e Azam";
+var message: string = "With faith, discipline and selfless devotion to duty, there is nothing worthwhile that you cannot achieve."
+console.log(message);
