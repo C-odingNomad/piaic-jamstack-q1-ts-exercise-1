cc490575ccb30078c8df4cc1e78e12aeb54d91ae
C-odingNomad <zeeblak@yahoo.com>
2023-11-10 01:53:13 +0500

    Completed 11th Exercise

---
+++ ex11/index.js
@@ -0,0 +1,5 @@
+var modeOfTransportation = ["Bicycle", "Honda", "Toyota Corolla", "Tesla Model X"];
+console.log("I would like to own a ".concat(modeOfTransportation[0], "."));
+console.log("I would like to own a ".concat(modeOfTransportation[1], "."));
+console.log("I would like to own a ".concat(modeOfTransportation[2], "."));
+console.log("I would like to own a ".concat(modeOfTransportation[3], "."));
---
+++ ex11/index.ts
@@ -0,0 +1,7 @@
+
+
+var modeOfTransportation: string[] = ["Bicycle", "Honda", "Toyota Corolla", "Tesla Model X"];
+console.log(`I would like to own a ${modeOfTransportation[0]}.`);
+console.log(`I would like to own a ${modeOfTransportation[1]}.`);
+console.log(`I would like to own a ${modeOfTransportation[2]}.`);
+console.log(`I would like to own a ${modeOfTransportation[3]}.`);
