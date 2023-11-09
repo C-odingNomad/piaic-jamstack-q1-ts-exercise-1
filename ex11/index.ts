ae453dbe2f9d2ffcbd39115573e6691a6d63b71b
C-odingNomad <zeeblak@yahoo.com>
2023-11-10 01:53:41 +0500

    Edit Something in Ex11

--- ex11/index.ts
+++ ex11/index.ts
@@ -1,4 +1,4 @@
-
+// Mode of Transportation:
 
 var modeOfTransportation: string[] = ["Bicycle", "Honda", "Toyota Corolla", "Tesla Model X"];
 console.log(`I would like to own a ${modeOfTransportation[0]}.`);
