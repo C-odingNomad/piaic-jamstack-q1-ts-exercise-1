af19e16ac26a6e682d57ef5f0f57c2226d5f8303
C-odingNomad <zeeblak@yahoo.com>
2023-11-10 01:38:09 +0500

    Completed 9th Exercise

---
+++ ex09/index.ts
@@ -0,0 +1,7 @@
+// Creating An Array of my friends. Accessing all element one-by-one one at a time.
+
+var namesOfFriends: string[] = ["Muhammad Ali", "Raza Hassan", "Shafi Hassan", "Ahmed Ali"];
+console.log(namesOfFriends[0]);
+console.log(namesOfFriends[1]);
+console.log(namesOfFriends[2]);
+console.log(namesOfFriends[3]);
