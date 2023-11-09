8a64120c6e67ba42c83157edd1b37d73abf14042
C-odingNomad <zeeblak@yahoo.com>
2023-11-10 01:46:21 +0500

    Completed 10th Exercise

---
+++ ex10/index.js
@@ -0,0 +1,6 @@
+// Greetings (Personalized Message):
+var namesOfFriends = ["Muhammad Ali", "Raza Hassan", "Shafi Hassan", "Ahmed Ali"];
+console.log("Hi ".concat(namesOfFriends[0], ", how are you?"));
+console.log("Hi ".concat(namesOfFriends[1], ", how are you?"));
+console.log("Hi ".concat(namesOfFriends[2], ", how are you?"));
+console.log("Hi ".concat(namesOfFriends[3], ", how are you?"));
---
+++ ex10/index.ts
@@ -0,0 +1,7 @@
+// Greetings (Personalized Message):
+
+var namesOfFriends: string[] = ["Muhammad Ali", "Raza Hassan", "Shafi Hassan", "Ahmed Ali"];
+console.log(`Hi ${namesOfFriends[0]}, how are you?`);
+console.log(`Hi ${namesOfFriends[1]}, how are you?`);
+console.log(`Hi ${namesOfFriends[2]}, how are you?`);
+console.log(`Hi ${namesOfFriends[3]}, how are you?`);
