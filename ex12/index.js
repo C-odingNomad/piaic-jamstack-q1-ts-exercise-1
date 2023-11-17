ded8c8b54e1ecc6d6800d4fd661e4fe008f5c383
C-odingNomad <zeeblak@yahoo.com>
2023-11-17 19:46:50 +0500

    Edited ex12

--- ex12/index.js
+++ ex12/index.js
@@ -1,8 +1,7 @@
+// Name: Zeeshan Hassan
+// Date: 13 / 11 / 2023
+// List of guests inviting them to a dinner:
 var guestList = ["Minahil", "Raza", "Ali", "Shafi"];
-// console.log(`Hi ${guests[0]}, I have arranged a dinner for you tonight. Your presence would be amazing !`);
-// console.log(`Hi ${guests[1]}, I have arranged a dinner for you tonight. Your presence would be amazing !`);
-// console.log(`Hi ${guests[2]}, I have arranged a dinner for you tonight. Your presence would be amazing !`);
-// console.log(`Hi ${guests[3]}, I have arranged a dinner for you tonight. Your presence would be amazing !`);
 for (var i = 0; i < guestList.length; i++) {
     console.log("Hi ".concat(guestList[i], ", I have arranged a dinner for you tonight. Your presence would be amazing !"));
 }
