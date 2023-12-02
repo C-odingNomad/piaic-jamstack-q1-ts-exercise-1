350861dfb727023128faa6305b9a90f91a1abeb6
C-odingNomad <zeeblak@yahoo.com>
2023-12-02 21:30:28 +0500

    Started ex13

--- ex13/index.js
+++ ex13/index.js
@@ -1,14 +1,11 @@
 // Name: Zeeshan Hassan
 // Date: 2 / 12 / 2023
-
 // Modifying Old Guest List:
-let guestList: string[] = ["Minahil", "Raza", "Ali", "Shafi"];
-let notAttending = "Minahil";
-let newGuest = "Fozia";
+var guestList = ["Minahil", "Raza", "Ali", "Shafi"];
+var notAttending = "Minahil";
+var newGuest = "Fozia";
 guestList[guestList.indexOf(notAttending)] = newGuest;
 for (var i = 0; i < guestList.length; i++) {
-  console.log(
-    `Hi ${guestList[i]}, You're invited to our home tonight for a dinner. Your presence would be amazing !`
-  );
+    console.log("Hi ".concat(guestList[i], ", You're invited to our home tonight for a dinner. Your presence would be amazing !"));
 }
 console.log(notAttending + " " + "Can't make it for the dinner tonight. ");
