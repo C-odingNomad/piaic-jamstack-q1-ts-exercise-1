fdb83728bdd4acb54803c94beda64e3463aeace6
C-odingNomad <zeeblak@yahoo.com>
2023-12-18 12:49:45 +0500

    Created ex17 and edit others

---
+++ ex18/index.js
@@ -0,0 +1,9 @@
+// Date: 18/12/2023
+// Zeeshan Hassan
+// Creating an Array and using it.
+var mobileBrands = [
+    "Oppo", "Realme", "Samsung", "Apple"
+];
+mobileBrands.map(function (item) {
+    console.log("Mobile Phone Brand: ".concat(item, " "));
+});
---
+++ ex18/index.ts
@@ -0,0 +1,11 @@
+// Date: 18/12/2023
+// Zeeshan Hassan
+
+// Creating an Array and using it.
+let mobileBrands = [
+    "Oppo", "Realme", "Samsung", "Apple"
+];
+mobileBrands.map((item) =>{
+    console.log(`Mobile Phone Brand: ${item} `)
+
+})
