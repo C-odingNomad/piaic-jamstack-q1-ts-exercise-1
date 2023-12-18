661221c11e8e2d58e421b0662d2919c4132a8983
C-odingNomad <zeeblak@yahoo.com>
2023-12-18 12:07:51 +0500

    Created ex16

---
+++ ex16/index.js
@@ -0,0 +1,5 @@
+// Date: 18/12/2023
+// Zeeshan Hassan
+// indicating the number of people we are inviting to dinner:-
+var guestList = ["Minahil", "Raza", "Ali", "Shafi"];
+console.log("".concat(guestList.length, " people are being invited to the Dinner."));
---
+++ ex16/index.ts
@@ -0,0 +1,8 @@
+// Date: 18/12/2023
+// Zeeshan Hassan
+
+// indicating the number of people we are inviting to dinner:-
+
+
+let guestList: string[] = ["Minahil", "Raza", "Ali", "Shafi"];
+console.log(`${guestList.length} people are being invited to the Dinner.`)
