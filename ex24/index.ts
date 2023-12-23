a8df7641bdb23badb3f68b8c37a679d2343e2b6b
C-odingNomad <zeeblak@yahoo.com>
2023-12-23 17:09:13 +0500

    created ex24

---
+++ ex24/index.ts
@@ -0,0 +1,13 @@
+// Date: 23/12/2023
+// Zeeshan Hassan
+
+// Alien Color # 2  : Creating if-else chain:-
+
+
+let alien_color = "green";
+if (alien_color === "green") {
+    console.log("Congrats!! You just earned 5 points, because you shoot green alien.")
+}
+else {
+    console.log("Congrats!! You just earned 10 points.")
+}
