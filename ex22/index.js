4125b92eb7a3d81c35fb66e6b6d8ddcdf8a91231
C-odingNomad <zeeblak@yahoo.com>
2023-12-23 15:35:23 +0500

    created ex22

---
+++ ex22/index.js
@@ -0,0 +1,25 @@
+// Date: 23/12/2023
+// Zeeshan Hassan
+// Creating Some More Conditionals Tests:-
+// Testing 2 Strings:-
+// let str1 = "Laptop";
+// let str2 = "Computer";
+// console.log(str1 === str2);
+// console.log(str1 !== str2);
+// // Testing Using Lower Case:-
+// let str3 = "For Example";
+// console.log(str3.toLowerCase() === "for example");
+// Testing Numerical Values:-
+// let num1 = 5;
+// let num2 = 10;
+// console.log("5 !== 10 : ", num1 !== num2)
+// console.log("5 === 10 : ", num1 === num2)
+// console.log("5 > 10 : ", num1 > num2)
+// console.log("5 < 10 : ", num1 < num2)
+// console.log("5 <= 10 : ", num1 <= num2)
+// console.log("5 >= 10 : ", num1 >= num2)
+// Testing Conditions Using " && " and " || ":-
+var isIamFat = false;
+var isIamSmart = true;
+console.log(isIamFat && isIamSmart);
+console.log(isIamFat || isIamSmart);
