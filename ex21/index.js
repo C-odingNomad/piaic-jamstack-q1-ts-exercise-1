be1c217e0351130bad3c6e8f90f1aa073c85d967
C-odingNomad <zeeblak@yahoo.com>
2023-12-23 15:11:35 +0500

    created ex21

---
+++ ex21/index.js
@@ -0,0 +1,37 @@
+// Date: 23/12/2023
+// Zeeshan Hassan
+// Creating 10 Conditional Tests with True and False:
+// True Conditions:-
+console.log("True Conditions:- ");
+var fountainPen = "Dux";
+console.log("Is fountainPen == Dux ? I predict True for 'Dux'. Otherwise False");
+console.log(fountainPen === "Dux");
+var wristWatch = "Rolex";
+console.log("Is wristWatch == Rolex ? I predict True for 'Rolex'. Otherwise False");
+console.log(wristWatch === "Rolex");
+var tableMaterial = "Wood";
+console.log("Is tableMaterial == Wood ? I predict True for 'Wood'. Otherwise False");
+console.log(tableMaterial === "Wood");
+var laptopBrand = "Dell";
+console.log("Is laptopBrand == Dell ? I predict True for 'Dell'. Otherwise False");
+console.log(laptopBrand === "Dell");
+var softDrink = "Pakola";
+console.log("Is softDrink == Pakola ? I predict True for 'Pakola'. Otherwise False");
+console.log(softDrink === "Pakola");
+// False Conditions:-
+console.log("False Conditions:- ");
+var slipperMaterial = "Rubber";
+console.log("Is slipperMaterial == Rubber ? I predict True for 'Rubber'. Otherwise False");
+console.log(slipperMaterial === "Leather");
+var carpetColor = "Red";
+console.log("Is carpetColor == Red ? I predict True for 'Red'. Otherwise False");
+console.log(carpetColor === "Black");
+var mobileBrand = "Oppo";
+console.log("Is mobileBrand == Oppo ? I predict True for 'Oppo'. Otherwise False");
+console.log(mobileBrand === "Apple");
+var ceilingFan = "Asia Fans";
+console.log("Is ceilingFan == Asia Fans ? I predict True for 'Asia Fans'. Otherwise False");
+console.log(ceilingFan === "Pak Fans");
+var book = "Deep Work";
+console.log("Is book == Deep Work ? I predict True for 'Deep Work'. Otherwise False");
+console.log(book === "Fast Lane Millionaire");
---
+++ ex21/index.ts
@@ -0,0 +1,71 @@
+// Date: 23/12/2023
+// Zeeshan Hassan
+
+// Creating 10 Conditional Tests with True and False:
+
+// True Conditions:-
+
+console.log("True Conditions:- ");
+
+let fountainPen = "Dux";
+console.log(
+  "Is fountainPen == Dux ? I predict True for 'Dux'. Otherwise False"
+);
+console.log(fountainPen === "Dux");
+
+let wristWatch = "Rolex";
+console.log(
+  "Is wristWatch == Rolex ? I predict True for 'Rolex'. Otherwise False"
+);
+console.log(wristWatch === "Rolex");
+
+let tableMaterial = "Wood";
+console.log(
+  "Is tableMaterial == Wood ? I predict True for 'Wood'. Otherwise False"
+);
+console.log(tableMaterial === "Wood");
+
+let laptopBrand = "Dell";
+console.log(
+  "Is laptopBrand == Dell ? I predict True for 'Dell'. Otherwise False"
+);
+console.log(laptopBrand === "Dell");
+
+let softDrink = "Pakola";
+console.log(
+  "Is softDrink == Pakola ? I predict True for 'Pakola'. Otherwise False"
+);
+console.log(softDrink === "Pakola");
+
+// False Conditions:-
+console.log("False Conditions:- ");
+
+let slipperMaterial = "Rubber";
+console.log(
+  "Is slipperMaterial == Rubber ? I predict True for 'Rubber'. Otherwise False"
+);
+console.log(slipperMaterial === "Leather");
+
+let carpetColor = "Red";
+console.log(
+  "Is carpetColor == Red ? I predict True for 'Red'. Otherwise False"
+);
+console.log(carpetColor === "Black");
+
+let mobileBrand = "Oppo";
+console.log(
+  "Is mobileBrand == Oppo ? I predict True for 'Oppo'. Otherwise False"
+);
+console.log(mobileBrand === "Apple");
+
+let ceilingFan = "Asia Fans";
+console.log(
+  "Is ceilingFan == Asia Fans ? I predict True for 'Asia Fans'. Otherwise False"
+);
+console.log(ceilingFan === "Pak Fans");
+
+let book = "Deep Work";
+console.log(
+  "Is book == Deep Work ? I predict True for 'Deep Work'. Otherwise False"
+);
+console.log(book === "Fast Lane Millionaire");
