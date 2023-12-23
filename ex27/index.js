ffe3d165788ebc04bd93b1c102a1fa25ab907370
C-odingNomad <zeeblak@yahoo.com>
2023-12-23 18:08:26 +0500

    created ex27

---
+++ ex27/index.js
@@ -0,0 +1,20 @@
+// Date: 23/12/2023
+// Zeeshan Hassan
+// Searching specific fruit in my array of fruits:-
+var fav_fruits = ["mango", "strawberry", "pineapple", "banana"];
+// Check for specific fruits using if statements
+if (fav_fruits.includes("banana")) {
+    console.log("You really like bananas!");
+}
+else if (fav_fruits.includes("apple")) {
+    console.log("An apple a day keeps the doctor away!");
+}
+else if (fav_fruits.includes("orange")) {
+    console.log("Orange you glad you like oranges?");
+}
+else if (fav_fruits.includes("grape")) {
+    console.log("Grape choice! Grapes are delicious.");
+}
+else if (fav_fruits.includes("watermelon")) {
+    console.log("Watermelon is the perfect summer fruit!");
+}
---
+++ ex27/index.ts
@@ -0,0 +1,19 @@
+// Date: 23/12/2023
+// Zeeshan Hassan
+
+// Searching specific fruit in my array of fruits:-
+const fav_fruits: string[] = ["mango", "strawberry", "pineapple", "banana"];
+
+// Searching specific fruits using if statements:-
+
+if (fav_fruits.includes("banana")) {
+  console.log("You really like bananas!");
+} else if (fav_fruits.includes("apple")) {
+  console.log("An apple a day keeps the doctor away!");
+} else if (fav_fruits.includes("orange")) {
+  console.log("Orange you glad you like oranges?");
+} else if (fav_fruits.includes("grape")) {
+  console.log("Grape choice! Grapes are delicious.");
+} else if (fav_fruits.includes("watermelon")) {
+  console.log("Watermelon is the perfect summer fruit!");
+}
