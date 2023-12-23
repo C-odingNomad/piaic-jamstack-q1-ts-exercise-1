1560a895ea93355af7de9e781ea06a03176bf20f
C-odingNomad <zeeblak@yahoo.com>
2023-12-23 17:23:30 +0500

    created ex25

---
+++ ex25/index.ts
@@ -0,0 +1,13 @@
+// Date: 23/12/2023
+// Zeeshan Hassan
+
+// Alien # 3 : Creating a if-else chain:-
+
+let alien_color = "green";
+if (alien_color === "green") {
+  console.log("Congrats!! The player just earned 5 points. ");
+} else if (alien_color === "yellow") {
+  console.log("The player just earned 10");
+} else if (alien_color === "red") {
+  console.log("The player just earned 15");
+}
