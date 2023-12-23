754b5a8bc82881e422cfc6f05e593f71525d6ec2
C-odingNomad <zeeblak@yahoo.com>
2023-12-23 14:50:34 +0500

    created ex20

---
+++ ex20/index.js
@@ -0,0 +1,5 @@
+// Date: 23/12/2023
+// Zeeshan Hassan
+// Creating an itentional error:
+var fruit = ["Apple", "Mango", "Banana", "Orange"];
+console.log(fruit[20]);
---
+++ ex20/index.ts
@@ -0,0 +1,8 @@
+// Date: 23/12/2023
+// Zeeshan Hassan
+
+// Creating an itentional error:
+let fruit = ["Apple", "Mango", "Banana", "Orange"];
+
+// Uncomment below statement to print an error:
+// console.log(fruit[20]);
