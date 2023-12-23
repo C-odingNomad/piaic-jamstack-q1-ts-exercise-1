605c9c090dd3eaf55372a5d64194e40daf3a72c8
C-odingNomad <zeeblak@yahoo.com>
2023-12-23 16:11:58 +0500

    created final ex22

--- ex22/index.js
+++ ex22/index.js
@@ -2,24 +2,28 @@
 // Zeeshan Hassan
 // Creating Some More Conditionals Tests:-
 // Testing 2 Strings:-
-// let str1 = "Laptop";
-// let str2 = "Computer";
-// console.log(str1 === str2);
-// console.log(str1 !== str2);
-// // Testing Using Lower Case:-
-// let str3 = "For Example";
-// console.log(str3.toLowerCase() === "for example");
+var str1 = "Laptop";
+var str2 = "Computer";
+console.log(str1 === str2);
+console.log(str1 !== str2);
+// Testing Using Lower Case:-
+var str3 = "For Example";
+console.log(str3.toLowerCase() === "for example");
 // Testing Numerical Values:-
-// let num1 = 5;
-// let num2 = 10;
-// console.log("5 !== 10 : ", num1 !== num2)
-// console.log("5 === 10 : ", num1 === num2)
-// console.log("5 > 10 : ", num1 > num2)
-// console.log("5 < 10 : ", num1 < num2)
-// console.log("5 <= 10 : ", num1 <= num2)
-// console.log("5 >= 10 : ", num1 >= num2)
+var num1 = 5;
+var num2 = 10;
+console.log("5 !== 10 : ", num1 !== num2);
+console.log("5 === 10 : ", num1 === num2);
+console.log("5 > 10 : ", num1 > num2);
+console.log("5 < 10 : ", num1 < num2);
+console.log("5 <= 10 : ", num1 <= num2);
+console.log("5 >= 10 : ", num1 >= num2);
 // Testing Conditions Using " && " and " || ":-
 var isIamFat = false;
 var isIamSmart = true;
 console.log(isIamFat && isIamSmart);
 console.log(isIamFat || isIamSmart);
+// Testing the presence of an item in an array:-
+var fruits = ["apple", "mango", "banana", "kiwi"];
+// As I can't use ".include()" func, so I'm using .indexof() :-
+console.log("If 'kiwi' is present in above array it'll show it's index : ", fruits.indexOf("kiwi"));
--- ex22/index.ts
+++ ex22/index.ts
@@ -5,28 +5,36 @@
 
 // Testing 2 Strings:-
 
-// let str1 = "Laptop";
-// let str2 = "Computer";
-// console.log(str1 === str2);
-// console.log(str1 !== str2);
+let str1 = "Laptop";
+let str2 = "Computer";
+console.log(str1 === str2);
+console.log(str1 !== str2);
 
-// // Testing Using Lower Case:-
-// let str3 = "For Example";
-// console.log(str3.toLowerCase() === "for example");
+// Testing Using Lower Case:-
+let str3 = "For Example";
+console.log(str3.toLowerCase() === "for example");
 
 // Testing Numerical Values:-
-// let num1 = 5;
-// let num2 = 10;
-// console.log("5 !== 10 : ", num1 !== num2)
-// console.log("5 === 10 : ", num1 === num2)
-// console.log("5 > 10 : ", num1 > num2)
-// console.log("5 < 10 : ", num1 < num2)
-// console.log("5 <= 10 : ", num1 <= num2)
-// console.log("5 >= 10 : ", num1 >= num2)
-
+let num1 = 5;
+let num2 = 10;
+console.log("5 !== 10 : ", num1 !== num2);
+console.log("5 === 10 : ", num1 === num2);
+console.log("5 > 10 : ", num1 > num2);
+console.log("5 < 10 : ", num1 < num2);
+console.log("5 <= 10 : ", num1 <= num2);
+console.log("5 >= 10 : ", num1 >= num2);
 
 // Testing Conditions Using " && " and " || ":-
 let isIamFat = false;
 let isIamSmart = true;
 console.log(isIamFat && isIamSmart);
 console.log(isIamFat || isIamSmart);
+
+// Testing the presence of an item in an array:-
+const fruits: string[] = ["apple", "mango", "banana", "kiwi"];
+
+// As I can't use ".include()" func, so I'm using .indexof() :-
+console.log(
+  "If 'kiwi' is present in above array it'll show it's index : ",
+  fruits.indexOf("kiwi")
+);
