df9fb1b6027a6799e8c1b9259b69a6429d4fa280
C-odingNomad <zeeblak@yahoo.com>
2023-12-23 14:47:42 +0500

    created ex19

---
+++ ex19/index.js
@@ -0,0 +1,10 @@
+// Date: 23/12/2023
+// Zeeshan Hassan
+// Creating an Object:
+var mySmartPhone = {
+    brand: "Oppo",
+    year: 2023,
+    color: "Black",
+    specs: "64GB RAM and 4GB ROM",
+};
+console.log(mySmartPhone);
---
+++ ex19/index.ts
@@ -0,0 +1,16 @@
+// Date: 23/12/2023
+// Zeeshan Hassan
+
+// Creating an Object:
+const mySmartPhone: {
+  brand: string;
+  year: number;
+  color: string;
+  specs: string;
+} = {
+  brand: "Oppo",
+  year: 2023,
+  color: "Black",
+  specs: "64GB RAM and 4GB ROM",
+};
+console.log(mySmartPhone);
