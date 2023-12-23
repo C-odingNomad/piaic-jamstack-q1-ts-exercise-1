daf3e547b677b2a88e7e57a7ddc9bbe05498f197
C-odingNomad <zeeblak@yahoo.com>
2023-12-23 16:27:11 +0500

    created ex23

---
+++ ex23/index.js
@@ -0,0 +1,13 @@
+// Date: 23/12/2023
+// Zeeshan Hassan
+// Alien Color #1 : Creating 2 if Statements:
+// Version of a program that will execute:-
+var alien_color = "green";
+if (alien_color === "green") {
+    console.log("🥳 You just earned 5 points !!");
+}
+// Version of a program that won't execute:-
+alien_color = "red";
+if (alien_color === "yellow") {
+    console.log("Try Again !!");
+}
---
+++ ex23/index.ts
@@ -0,0 +1,18 @@
+// Date: 23/12/2023
+// Zeeshan Hassan
+
+// Alien Color #1 : Creating 2 if Statements:
+
+// Version of a program that will execute:-
+
+let alien_color = "green";
+if (alien_color === "green") {
+  console.log("🥳 You just earned 5 points !!");
+}
+
+// Version of a program that won't execute:-
+
+alien_color = "red";
+if (alien_color === "yellow") {
+  console.log("Try Again !!");
+}
