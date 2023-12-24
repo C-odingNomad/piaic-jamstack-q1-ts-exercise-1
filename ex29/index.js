f0c250e1ad7ff69a8267f1cd33b0175f8b35aa5b
C-odingNomad <zeeblak@yahoo.com>
2023-12-24 15:28:56 +0500

    created ex29

---
+++ ex29/index.js
@@ -0,0 +1,9 @@
+// Date: 24/12/2023
+// Zeeshan Hassan
+// No Users:- Checking the Empty List (array)
+var users = ["iman", "john", "michael", "admin", "alex"];
+users.splice(0, 5);
+// console.log(users)
+if (users.length === 0) {
+    console.log("We need to find some users!");
+}
---
+++ ex29/index.ts
@@ -0,0 +1,10 @@
+// Date: 24/12/2023
+// Zeeshan Hassan
+
+// No Users:- Checking the Empty List (array)
+
+const users = ["iman", "john", "michael", "admin", "alex"];
+users.splice(0, 5);
+if (users.length === 0) {
+  console.log("We need to find some users!");
+}
