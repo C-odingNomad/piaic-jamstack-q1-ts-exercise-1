572600ba9cf5dbbe78e5b07e7641a8ea881d5e85
C-odingNomad <zeeblak@yahoo.com>
2023-12-26 17:54:16 +0500

    created ex34

---
+++ ex34/index.js
@@ -0,0 +1,7 @@
+// Date: 26/12/2023
+// Zeeshan Hassan
+// Creating a function for making shirts:
+function make_shirt(size, message) {
+    console.log("".concat(size, "inch is the size of this shirt and \"").concat(message, "\" is the message which will be print on this shirt."));
+}
+make_shirt(5, "Hello World");
---
+++ ex34/index.ts
@@ -0,0 +1,8 @@
+// Date: 26/12/2023
+// Zeeshan Hassan
+
+// Creating a function for making shirts:
+function make_shirt(size:number, message: string) {
+    console.log(`${size}inch is the size of this shirt and "${message}" is the message which will be print on this shirt.`)
+}
+make_shirt(5,"Hello World");
