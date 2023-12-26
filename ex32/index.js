dea601b91611e60c75748e899bec3b923cbce0a9
C-odingNomad <zeeblak@yahoo.com>
2023-12-26 17:19:09 +0500

    created ex32

---
+++ ex32/index.js
@@ -0,0 +1,12 @@
+// Date: 24/12/2023
+// Zeeshan Hassan
+// Pizzas: Program for printing the message for my favorite pizzas with their names.
+var favPizzas = ["Creamy Cheese", "Chicken Fagita", "Afghani Kabab"];
+// for (let i=0; i < favPizzas.length; i++) {
+//     console.log(favPizzas[i]);
+// }
+for (var _i = 0, favPizzas_1 = favPizzas; _i < favPizzas_1.length; _i++) {
+    var pizza = favPizzas_1[_i];
+    console.log("I like ".concat(pizza, " pizza."));
+}
+console.log("I really love pizza! 😅");
---
+++ ex32/index.ts
@@ -0,0 +1,12 @@
+// Date: 24/12/2023
+// Zeeshan Hassan
+// Pizzas: Program for printing the message for my favorite pizzas with their names.
+
+const favPizzas: string[] = ["Creamy Cheese", "Chicken Fagita", "Afghani Kabab"];
+// for (let i=0; i < favPizzas.length; i++) {
+//     console.log(favPizzas[i]);
+// }
+for (let pizza of favPizzas) {
+    console.log(`I like ${pizza} pizza.`)
+}
+console.log("I really love pizza! 😅")
